92df0f7f76cf7b23234edb743f51fa7df0953d85
Paulo Victor Huertas <paulohuertas@outlook.com>
2023-04-22 19:11:08 +0100

    Add project files.

---
+++ ReadXml/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ReadXml
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_FilePath = new System.Windows.Forms.TextBox();
+            this.lb_FilePath = new System.Windows.Forms.Label();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.btn_ParseFile = new System.Windows.Forms.Button();
+            this.txt_Output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_FilePath
+            // 
+            this.txt_FilePath.Location = new System.Drawing.Point(55, 71);
+            this.txt_FilePath.Name = "txt_FilePath";
+            this.txt_FilePath.Size = new System.Drawing.Size(500, 22);
+            this.txt_FilePath.TabIndex = 0;
+            // 
+            // lb_FilePath
+            // 
+            this.lb_FilePath.AutoSize = true;
+            this.lb_FilePath.Location = new System.Drawing.Point(55, 49);
+            this.lb_FilePath.Name = "lb_FilePath";
+            this.lb_FilePath.Size = new System.Drawing.Size(63, 17);
+            this.lb_FilePath.TabIndex = 1;
+            this.lb_FilePath.Text = "File Path";
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Location = new System.Drawing.Point(583, 69);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(75, 23);
+            this.btn_Search.TabIndex = 2;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // btn_ParseFile
+            // 
+            this.btn_ParseFile.Location = new System.Drawing.Point(676, 69);
+            this.btn_ParseFile.Name = "btn_ParseFile";
+            this.btn_ParseFile.Size = new System.Drawing.Size(75, 23);
+            this.btn_ParseFile.TabIndex = 3;
+            this.btn_ParseFile.Text = "Parse";
+            this.btn_ParseFile.UseVisualStyleBackColor = true;
+            this.btn_ParseFile.Click += new System.EventHandler(this.btn_ParseFile_Click);
+            // 
+            // txt_Output
+            // 
+            this.txt_Output.Location = new System.Drawing.Point(58, 188);
+            this.txt_Output.Multiline = true;
+            this.txt_Output.Name = "txt_Output";
+            this.txt_Output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_Output.Size = new System.Drawing.Size(693, 206);
+            this.txt_Output.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 450);
+            this.Controls.Add(this.btn_ParseFile);
+            this.Controls.Add(this.btn_Search);
+            this.Controls.Add(this.lb_FilePath);
+            this.Controls.Add(this.txt_Output);
+            this.Controls.Add(this.txt_FilePath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_FilePath;
+        private System.Windows.Forms.Label lb_FilePath;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.Button btn_ParseFile;
+        private System.Windows.Forms.TextBox txt_Output;
+    }
+}
+
---
+++ ReadXml/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ReadXml
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if(DialogResult.OK == openFileDialog.ShowDialog())
+            {
+                if(openFileDialog.FileName.Length > 0)
+                    txt_FilePath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void btn_ParseFile_Click(object sender, EventArgs e)
+        {
+            XmlDocument xml = new XmlDocument();
+
+            if (!String.IsNullOrEmpty(txt_FilePath.Text))
+            {
+                //Dictionary<string, string> valuePairs = new Dictionary<string, string>();
+                xml.Load(txt_FilePath.Text);
+                XmlNamespaceManager xmlNamespace = new XmlNamespaceManager(xml.NameTable);
+
+                var att = xml.DocumentElement.Attributes;
+                if(att.Count > 0)
+                {
+                    for(int i = 0; i < att.Count; i++)
+                    {
+                        string prefix = att[i].Name.Substring(att[i].Name.IndexOf(":") + 1);
+                        string uri = att[i].Value;
+
+                        if (prefix.StartsWith("ns"))
+                        {
+                            xmlNamespace.AddNamespace(prefix, uri);
+                        }
+                    } 
+                }
+                List<DataUpdater> dataUpdaterList = new List<DataUpdater>();
+
+                var parentNodeList = xml.SelectNodes("//ns0:RDEntityList//ns2:RDEntity", xmlNamespace);
+
+                string displayText = String.Empty;
+
+                if (parentNodeList.Count > 0)
+                {
+                    List<XmlDocument> docsToSave = new List<XmlDocument>();
+                    for (int i = 0; i < parentNodeList.Count; i++)
+                    {
+                        string codeListName = String.Empty;
+                        string codeCode = String.Empty;
+                        string codeValue = String.Empty;
+                        string codeDescription = String.Empty;
+
+                        if (!String.IsNullOrEmpty(parentNodeList[i].Attributes[0].InnerText))
+                        {
+                            codeListName = parentNodeList[i].Attributes[0].InnerText;
+                        }
+
+                        var nodeCollection = parentNodeList[i].ChildNodes;
+                        for (int j = 0; j < nodeCollection.Count; j++)
+                        {
+                            var child = nodeCollection[j].ChildNodes;
+                            foreach(XmlNode node in child)
+                            {
+                                if (node.Name == "ns4:dataItem")
+                                {
+                                    codeCode = node.InnerText;
+                                    codeValue = node.InnerText;
+
+                                } 
+                                else if(node.Name == "ns4:LsdList")
+                                {
+                                    if(node.ChildNodes.Count > 0)
+                                    {
+                                        var descriptionChildNodes = node.ChildNodes;
+                                        int count = 0;
+                                        foreach(XmlNode nodeDesc in descriptionChildNodes)
+                                        {
+                                            if (nodeDesc.Attributes["lang"].Value == "en")
+                                            {
+                                                codeDescription = nodeDesc.InnerText;
+                                            }
+                                        }
+                                    }
+                                    
+                                }
+                                else
+                                {
+                                    codeDescription = codeCode.ToUpper().Trim();
+                                }
+                            }
+
+                            if(codeValue != String.Empty && codeDescription != String.Empty)
+                            {
+                                DataUpdater dataUpdater = new DataUpdater(codeListName, codeCode, codeValue, codeDescription);
+                                if (dataUpdater != null)
+                                {
+                                    dataUpdaterList.Add(dataUpdater);
+                                }
+                            }
+                        }
+
+                        string codeListDescription = codeListName + "_AES";
+
+                        var doc = CreateXmlTemplate(codeListName, codeListDescription);
+
+                        var newNode = CreateXmlNodeValues(doc, dataUpdaterList);
+
+                        var codeList = doc.SelectSingleNode("//Code");
+                        codeList.AppendChild(newNode);
+
+                        docsToSave.Add(doc);
+
+                        dataUpdaterList.Clear();
+                    }
+
+                    foreach (XmlDocument doc in docsToSave)
+                    {
+                        txt_Output.Text += doc.OuterXml;
+                        try
+                        {
+                            SaveFile(doc);
+                            string docFile = doc.SelectSingleNode("//DataUpdater//CodeList//Code//ReferenceCode").InnerText;
+                            MessageBox.Show($"File salvo com sucesso: {docFile.ToUpper()}");
+                        }
+                        catch (Exception)
+                        {
+                            string docFile = doc.SelectSingleNode("//DataUpdater//CodeList//Code//ReferenceCode").InnerText;
+                            throw new Exception($"Unable to save file: {docFile.ToUpper()}");
+                        }
+                        
+                        
+                    }
+
+                    this.Close();
+                }
+            }
+        }
+
+        public static void SaveFile(XmlDocument xmlDocument)
+        {
+            if (xmlDocument != null)
+            {
+                string directory = "C:\\01. Paulo Huertas\\c#\\Expeditors\\";
+                string docFile = xmlDocument.SelectSingleNode("//DataUpdater//CodeList//Code//ReferenceCode").InnerText;
+                string fileName = directory + docFile + ".xml";
+                xmlDocument.Save(fileName);
+            }
+        }
+
+        public static XmlDocument CreateXmlTemplate(string referenceCode, string descriptionCode)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlNode root = xmlDocument.CreateNode(XmlNodeType.Element, "DataUpdater", null);
+            xmlDocument.AppendChild(root);
+
+            XmlNode codeList = xmlDocument.CreateNode(XmlNodeType.Element, "CodeList", null);
+            root.AppendChild(codeList);
+
+            XmlNode code = xmlDocument.CreateNode(XmlNodeType.Element, "Code", null);
+            XmlAttribute xmlAttribute = xmlDocument.CreateAttribute("full");
+            xmlAttribute.Value = "Y";
+            code.Attributes.Append(xmlAttribute);
+
+            codeList.AppendChild(code);
+
+            XmlElement refCode = xmlDocument.CreateElement("ReferenceCode");
+            if (!String.IsNullOrEmpty(referenceCode))
+            {
+                refCode.InnerText = referenceCode;
+                code.AppendChild(refCode);
+            }
+
+            Dictionary<string, string> metdaDataNodes = new Dictionary<string, string>();
+            metdaDataNodes.Add("CodeConstraint", "2");
+            metdaDataNodes.Add("ValueConstraint", "2");
+            metdaDataNodes.Add("Qual1Constraint", "1");
+            metdaDataNodes.Add("Qual2Constraint", "0");
+            metdaDataNodes.Add("Qual3Constraint", "0");
+            metdaDataNodes.Add("Description", descriptionCode);
+
+            foreach (var kvp in metdaDataNodes)
+            {
+                XmlNode metaData = xmlDocument.CreateNode(XmlNodeType.Element, kvp.Key, null);
+                metaData.InnerText = kvp.Value;
+                if (metaData.Name == "Description")
+                {
+                    var att = xmlDocument.CreateAttribute("lang");
+                    att.Value = "en";
+                    metaData.Attributes.Prepend(att);
+                }
+                code.AppendChild(metaData);
+            }
+
+            XmlNode codeValueListNode = xmlDocument.CreateNode(XmlNodeType.Element, "CodeValueList", null);
+            code.AppendChild(codeValueListNode);
+
+            return xmlDocument;
+        }
+
+        public static XmlNode CreateXmlNodeValues(XmlDocument xmldoc, List<DataUpdater> updater)
+        {
+            try
+            {
+                if (xmldoc != null && updater.Count > 0)
+                {
+
+                    XmlNode codeValueListNode = xmldoc.LastChild.SelectSingleNode("//CodeValueList");
+
+                    if (updater.Count > 0)
+                    {
+                        for (int i = 0; i < updater.Count; i++)
+                        {
+                            XmlNode codeValue = xmldoc.CreateNode(XmlNodeType.Element, "CodeValue", null);
+
+                            XmlElement valueCode = xmldoc.CreateElement("ValueCode");
+                            valueCode.InnerText = updater[i].Code;
+                            codeValue.AppendChild(valueCode);
+
+                            XmlElement valueValue = xmldoc.CreateElement("ValueValue");
+                            valueValue.InnerText = updater[i].Value;
+                            codeValue.AppendChild(valueValue);
+
+                            XmlElement description = xmldoc.CreateElement("Description");
+                            XmlAttribute xmlAttribute = xmldoc.CreateAttribute("lang");
+                            xmlAttribute.Value = "en";
+                            description.Attributes.Append(xmlAttribute);
+                            description.InnerText = updater[i].Description;
+
+                            codeValue.AppendChild(description);
+
+                            codeValueListNode.AppendChild(codeValue);
+                        }
+                    }
+
+                    return codeValueListNode;
+                }
+                return null;
+            }
+            catch
+            {
+                throw new Exception("Failed to load Updater Element");
+            }
+        }
+
+        public class DataUpdater
+        {
+            public string Name { get; set; }
+            public string Code { get; set; }
+            public string Value { get; set; }
+            public string Description { get; set; }
+
+            public DataUpdater(string name, string code, string value, string description)
+            {
+                Name = name;
+                Code = code;
+                Value = value;
+                Description = description;
+            }
+        }
+    }
+}
